41d9dafdfad41448f2ae66a2495ec08334a45fd3
Ethan McCue <ethan@mccue.dev>
2025-10-27 13:44:37 -0400

    Glass bubble

--- src/Dialectric.java
+++ src/Dialectric.java
@@ -1,7 +1,7 @@
 import java.util.Optional;
 
 value class Dialectric implements Material {
-    double refractionIndex;
+    private double refractionIndex;
 
     Dialectric(double refractionIndex) {
         this.refractionIndex = refractionIndex;
@@ -15,10 +15,27 @@ value class Dialectric implements Material {
         var unit_direction = r_in.direction().unitVector();
         var refracted = Vec3.refract(unit_direction, rec.normal(), ri);
 
-        var scattered = new Ray(rec.p(), refracted);
+        double cos_theta = Math.min(unit_direction.multiply(-1).dotProduct(rec.normal()), 1.0);
+        double sin_theta = Math.sqrt(1.0 - cos_theta*cos_theta);
+
+        var cannot_refract = ri * sin_theta > 1.0;
+        Vec3 direction;
+        if (cannot_refract || reflectance(cos_theta, ri) > Math.random())
+            direction = Vec3.reflect(unit_direction, rec.normal());
+        else
+            direction = Vec3.refract(unit_direction, rec.normal(), ri);
+
+        var scattered = new Ray(rec.p(), direction);
         return Optional.of(new ScatteredRay(
                 attenuation,
                 scattered
         ));
+    }
+
+    private static double reflectance(double cosine, double refraction_index) {
+        // Use Schlick's approximation for reflectance.
+        var r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*Math.pow((1 - cosine),5);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,13 @@ void main() {
     var material_ground = new Lambertian(new Vec3(0.8, 0.8, 0.0));
     var material_center = new Lambertian(new Vec3(0.1, 0.2, 0.5));
     var material_left   = new Dialectric(1.50);
+    var material_bubble = new Dialectric(1.00 / 1.50);
     var material_right  = new Metal(new Vec3(0.8, 0.6, 0.2), 1.0);
 
     world.add(new Sphere(new Vec3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(new Sphere(new Vec3( 0.0,    0.0, -1.2),   0.5, material_center));
     world.add(new Sphere(new Vec3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(new Sphere(new Vec3(-1.0,    0.0, -1.0),   0.4, material_bubble));
     world.add(new Sphere(new Vec3( 1.0,    0.0, -1.0),   0.5, material_right));
 
 
