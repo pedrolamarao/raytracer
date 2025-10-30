be93cd33cc89767371c5228bc390f831b30e40ce
Pedro Lamarão <pedro.lamarao@gmail.com>
2025-10-30 19:12:14 -0300

    Remove unnecessary restriction.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,6 @@ tasks.compileJava.configure {
     javaCompiler = javaToolchains.compilerFor {
         languageVersion = JavaLanguageVersion.of(26)
         version = "26-jep401ea2+1-1"
-        vendor = JvmVendorSpec.ORACLE
     }
     options.isIncremental = false
     options.compilerArgs = listOf("--enable-preview")
@@ -29,7 +28,6 @@ tasks.run.configure {
     javaLauncher = javaToolchains.launcherFor {
         languageVersion = JavaLanguageVersion.of(26)
         version = "26-jep401ea2+1-1"
-        vendor = JvmVendorSpec.ORACLE
     }
     jvmArgs = listOf("--enable-preview")
 }
